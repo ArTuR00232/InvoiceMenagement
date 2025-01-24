3ebe107ed8b6336fb06bbca33c34a9b40309271b
Artur Oliveira <artur.oliveiraprado@gmail.com>
2025-01-24 15:15:57 -0300

    marker manipulation create and api update

--- Backend/API.py
+++ Backend/API.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import User, Code as restCode, Money, Months
+import User, Code as restCode, Money, Months, Marker
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder = '../front/app/src',static_url_path=('/'))
@@ -33,6 +33,11 @@ def getMonths(Iduser = ''):
     result = Months.consults(Iduser)
     return(result)
 
+@app.route('/API/Marker/<string:idUser>', methods = ['GET'])
+def getMarker(idUser):
+    result = Marker.consults(idUser)
+    return jsonify(result)
+
 #Methods to insert
 @app.route('/API/User/post/<string:username>+<string:passW>', methods = ['POST'])
 def setUser(username, passW):
@@ -45,16 +50,28 @@ def setMoney(Iduser, spend, date, description):
     result = Money.consults(Iduser)
     return(result)
 
+@app.route('/API/Marker/post/<string:idUser>+<string:name>+<string:color>', methods = ['POST'])
+def setMarker(idUser, name, color):
+    result = Marker.insert(idUser, name, color)
+    return (result)
+
+
 #Methods to update
 @app.route('/API/User/update/<string:Iduser>+<string:pw>+<string:code>', methods = ['POST'])
 def updatePass(Iduser, pw, code):
     result = User.update(pw, Iduser, code)
     return(result)
-@app.route('/API/Money/update/<string:Iduser>+<string:Id>+<string:spend>+<string:date>+<string:description>+<string:SpendOld>+<string:DateOld>', methods = ['POST'])
-def updateMoney(Iduser, Id, spend, date, description, SpendOld, DateOld):
-    Money.update(Iduser, Id, spend, date, description, SpendOld, DateOld)
+
+@app.route('/API/Money/update/<string:Iduser>+<string:Id>+<string:spend>+<string:date>+<string:marker>+<string:description>+<string:SpendOld>+<string:DateOld>', methods = ['POST'])
+def updateMoney(Iduser, Id, spend, date, marker, description, SpendOld, DateOld):
+    Money.update(Iduser, Id, spend, date, SpendOld, DateOld, marker,description)
     result = Money.consults(Iduser)
     return(result)
+
+@app.route('/API/Marker/update/<string:id>+<string:name>+<string:color>', methods = ['POST'])
+def updateMarker(id, name, color):
+    result = Marker.update(name, color, id)
+    return (result)
 
 
 #Methods to Delete
@@ -66,8 +83,14 @@ def delUser(idUser):
 
 @app.route('/API/Money/delete/<int:id>', methods = ['DELETE','OPTIONS'])
 def delMoney(id):
-    Money(id)
+    Money.delete(id)
     return(['TRUE'])
+
+@app.route('/API/Marker/delete/<int:id>', methods = ['DELETE','OPTIONS'])
+def delMarker(id):
+    Marker.delete(id)
+    return(['TRUE'])
+
 
 
 if __name__ == '__main__':
---
+++ Backend/Marker.py
@@ -0,0 +1,106 @@
+import Connect
+from flask import jsonify
+
+def consults(iduser, name = ''):
+    """
+    consult the marker table \n
+    Args\n:
+        iduser (int): user ID\n
+        name (str, optional): marker name. Defaults to ''.\n
+    Returns:\n
+        list: list with the info about the markers.
+    """
+    conn = Connect.DB()
+    cursor = conn.cursor()
+    if(name == ''):
+        query = ('SELECT id, name, color FROM marker WHERE idUser =?')
+        cursor.execute(query, (iduser,))
+        df = cursor.fetchall()
+        list_marker = [{
+            'id': row[0],
+            'name':row[1],
+            'color':row[2]
+        } for row in df]
+        return list_marker
+    if(name != ''):
+        print(iduser)
+        query = ('SELECT id, name, color FROM marker WHERE  idUser = ? AND name = ?')
+        cursor.execute(query,(iduser, name))
+        df = cursor.fetchall()
+        list_marker = [{
+            'id':row[0],
+            'name':row[1],
+            'color':row[2]
+        }for row in df]
+        if(df == []):
+            return False
+        else:
+            return (list_marker)
+        
+def insert(id, name, color):
+    """
+    Insert into the table marker \n
+    Args:\n
+        id (int): id user\n
+        name (str): name of the marker\n
+        color (str): hash of the color\n
+    """
+    conn = Connect.DB()
+    cursor = conn.cursor()
+    color = '#' + color
+    query = ('INSERT INTO marker (name, color, idUser) VALUES(?,?,?)')
+    cursor.execute(query,(name, color, id))
+    conn.commit()
+    conn.close()
+    cursor.close()
+    return(True)
+
+def update(name, color = '', id = ''):
+    """
+    Update the table marker\n
+    Args:\n
+        name (str): name of the marker.\n
+        color (str, optional): hash of the selected color. Defaults to ''.\n
+        id (int):  id od hte marker.
+
+    """
+    conn = Connect.DB()
+    cursor = conn.cursor()
+    query = 'UPDATE marker SET name = ?, color = ? WHERE id = ?'
+    cursor.execute(query,(name, color, id))
+    print('update')
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def delete(id):
+    """
+    module to delete from table marker\n
+    Args:\n
+        id (int): id of the marker.\n
+    Returns:\n
+        str: returs true when the marker is deleted
+    """
+    conn = Connect.DB()
+    cursor = conn.cursor()
+    query = 'DELETE FROM marker WHERE id = ?'
+    cursor.execute(query,(id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return ('True')
+
+def deleteAll(idUser):
+    """
+    Delete all the markers related with the current user.\n
+    Args:\n
+        idUser (int): user Id\n
+    """
+    conn = Connect.DB()
+    cursor = conn.cursor()
+    query = ('DELETE FROM marker WHERE idUser = ?')
+    cursor.execute(query,(idUser,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
